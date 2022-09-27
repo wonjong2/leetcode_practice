d4cd8d24af4c0a7e4ea177c7f2ded3d7c7154a89
Wonjong Park <wonjong2@gmail.com>
2022-09-26 21:47:26 -0700

    Improved 50.Pow(x,n)

--- 50_Pow(x,n)/index.js
+++ 50_Pow(x,n)/index.js
@@ -4,31 +4,21 @@
  * @return {number}
  */
 var myPow = function (x, n) {
-    if (n === 0) return 1;
+    const halfPow = (x, n) => {
+        if (n === 0) return 1;
 
-    if (n === 1) return x;
-    if (n === -1) return 1 / x;
+        let half = halfPow(x, Math.floor(n / 2));
 
-    let result = 1;
-
-    if (n % 2 === 0) {
-        for (let i = 0; i < Math.abs(n) / 2; i++) {
-            result = result * x;
-        }
-        result = result * result;
-    }
-    else {
-        for (let i = 0; i < Math.floor(Math.abs(n) / 2); i++) {
-            result = result * x;
-        }
-        result = result * result * x;
+        if (n % 2 === 0) return half * half;
+        return half * half * x;
     }
 
     if (n < 0) {
-        result = 1 / result;
+        x = 1 / x;
+        n = -n;
     }
 
-    return result;
+    return halfPow(x, n);
 };
 
 console.log(myPow(2.1, 3));
