052fe14172d5ab23b3ff49afa44c18d51cf92055
Wonjong Park <wonjong2@gmail.com>
2022-10-03 23:51:44 -0700

    Update the solution for #973

--- 973_K_Closest_Points_to_Origin_Medium/index.js
+++ 973_K_Closest_Points_to_Origin_Medium/index.js
@@ -4,34 +4,28 @@
  * @return {number[][]}
  */
 var kClosest = function (points, k) {
-    let distObj = {};
+    // let distObj = {};
     let distArray = [];
     let result = [];
 
     for (let i = 0; i < points.length; i++) {
-        let dist = (points[i][0]) ** 2 + (points[i][1]) ** 2;
-        let distStr = String(dist);
-        if (!distObj[String(dist)]) {
-            distObj[String(dist)] = [points[i]];
-            distArray.push(dist);
-        }
-        else {
-            distObj[String(dist)].push(points[i]);
-        }
+        let dist = distance(points[i]);
+        distArray.push(dist);
     }
 
     distArray.sort((a, b) => a - b);
 
-    let numOfSpots = 0;
-
-    for (let j = 0; j < k && numOfSpots < k; j++) {
-        for (let l = 0; l < distObj[String(distArray[j])]?.length; l++) {
-            result.push(distObj[String(distArray[j])][l]);
-            numOfSpots += distObj[String(distArray[j])]?.length;
+    for (let j = 0; j < points.length; j++) {
+        if (distance(points[j]) <= distArray[k - 1]) {
+            result.push(points[j]);
         }
     }
 
     return result;
+
+    function distance(spot) {
+        return spot[0] ** 2 + spot[1] ** 2;
+    }
 };
 
 //console.log(kClosest([[1, 3], [-2, 2]], 1)); // [[-2, 2]]
