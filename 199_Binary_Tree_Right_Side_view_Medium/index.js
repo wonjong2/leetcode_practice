0c037918c07fbc63b6f9fa0b1f4a9a9ae265187c
Wonjong Park <wonjong2@gmail.com>
2022-10-04 19:59:04 -0700

    Rename folder name

---
+++ 199_Binary_Tree_Right_Side_view_Medium/index.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function (root) {
+    if (!root) return [];
+
+    let result = [];
+
+    result.push(root.val);
+    if (root.right) {
+        getRightVal(root.right);
+    }
+
+    return result;
+
+    function getRightVal(node) {
+        result.push(node.val);
+        if (node.right) {
+            getRightVal(node.right);
+        }
+        return;
+    }
+};
