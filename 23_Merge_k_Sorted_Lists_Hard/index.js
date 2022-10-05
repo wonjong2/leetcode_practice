fd95b2e4453480bbeac39d649b8ea9ba7972fb66
Wonjong Park <wonjong2@gmail.com>
2022-10-05 12:23:40 -0700

    Update solution

---
+++ 23_Merge_k_Sorted_Lists_Hard/index.js
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
+
+// Time complexity : O(N) where N is the number of lists nodes
+// Space complexity : O(N) where N is the number of lists nodes
+var mergeKLists = function (lists) {
+    if (lists.length === 0) return null;
+
+    let valueArray = [];
+
+    for (let i = 0; i < lists.length; i++) {
+        while (lists[i]) {
+            valueArray.push(lists[i].val);
+            lists[i] = lists[i].next;
+        }
+    }
+
+    if (valueArray.length === 0) return null;
+
+    valueArray.sort((a, b) => a - b);
+
+    let result = {};
+
+    result.val = valueArray[0];
+    result.next = makeLinkedList(valueArray, 1);
+
+    return result;
+
+    function makeLinkedList(values, index) {
+        if (values[index] === undefined) return null;
+
+        let list = {};
+
+        list.val = values[index];
+        list.next = makeLinkedList(values, index + 1);
+
+        return list;
+    }
+}
