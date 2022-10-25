b81ba93f3d7f6bff02e7450b737750eb8c78f097
Wonjong Park <wonjong2@gmail.com>
2022-10-25 16:58:53 -0700

    Updates the solution for #69

--- 69_Sqrt_x_Easy/index.js
+++ 69_Sqrt_x_Easy/index.js
@@ -3,21 +3,14 @@
  * @return {number}
  */
 var mySqrt = function (x) {
-    if (x === 0) return 0;
-    if (x === 1) return 1;
+    if (x < 2) return x;
 
-    let temp = x;
-    let pow = 0;
-
-    while ((temp = Math.floor(temp / 2)) >= 1) {
-        pow++;
-    }
-
-    let higher = 2 ** ((Math.floor(pow / 2)) + 1);
+    let higher = Math.floor(x / 2);
     if (higher * higher === x) return higher;
 
-    let lower = 2 ** (Math.floor(pow / 2));
+    let lower = 2;
     if (lower * lower === x) return lower;
+    if (lower * lower > x) return 1;
 
     while (higher > lower) {
         let center = Math.floor((higher + lower) / 2);
@@ -35,6 +28,36 @@ var mySqrt = function (x) {
     }
 
     return lower;
+
+    // let temp = x;
+    // let pow = 0;
+
+    // while ((temp = Math.floor(temp / 2)) >= 1) {
+    //     pow++;
+    // }
+
+    // let higher = 2 ** ((Math.floor(pow / 2)) + 1);
+    // if (higher * higher === x) return higher;
+
+    // let lower = 2 ** (Math.floor(pow / 2));
+    // if (lower * lower === x) return lower;
+
+    // while (higher > lower) {
+    //     let center = Math.floor((higher + lower) / 2);
+    //     if (center * center == x) return center;
+    //     if (center * center > x) {
+    //         higher = center - 1;
+    //         if (higher * higher === x) return higher;
+    //         if (higher * higher === x) return center;
+    //     }
+    //     else {
+    //         lower = center + 1;
+    //         if (lower * lower === x) return lower;
+    //         if (lower * lower > x) return center;
+    //     }
+    // }
+
+    // return lower;
 };
 console.log(mySqrt(10));
 console.log(mySqrt(9));
