ece46d4f077a19ab6bc9e1a318f4554ffe10b4ec
Wonjong Park <wonjong2@gmail.com>
2022-10-27 17:08:23 -0700

    Updates 62.Unique Paths (Medium)

--- 62_Unique_Paths_Medium/index.js
+++ 62_Unique_Paths_Medium/index.js
@@ -4,13 +4,39 @@
  * @return {number}
  */
 var uniquePaths = function (m, n) {
-    let result = 0;
+    let subPaths = {};
+    //let result = 0;
 
-    goFinish(m, n);
+    return goFinish(0, 0);
 
-    return result;
+    //return result;
 
     function goFinish(low, column) {
+        if (subPaths[`(${low}, ${column})`]) return subPaths[`(${low}, ${column})`];
 
+        if (low === m - 1 && column === n - 1) {
+            return 1;
+        }
+
+        // if (low === m || column === n) return 0;
+
+        let result = 0;
+        let result2 = 0;
+
+        if (column < n - 1) {
+            result = goFinish(low, column + 1);
+            if (!subPaths[`(${low}, ${column + 1})`]) subPaths[`(${low}, ${column + 1})`] = result;
+        }
+
+        if (low < m - 1) {
+            result2 = goFinish(low + 1, column);
+            if (!subPaths[`(${low + 1}, ${column})`]) subPaths[`(${low + 1}, ${column})`] = result2;
+        }
+
+        return result + result2;;
     }
 };
+
+console.log(uniquePaths(4, 2));
+console.log(uniquePaths(23, 12));
+console.log(uniquePaths(3, 2));
