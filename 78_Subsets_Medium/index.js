082dba83e300b3a3ae90ea6cca19a6e295f3475e
Wonjong Park <wonjong2@gmail.com>
2022-11-15 16:45:05 -0800

    Updates the solution

--- 78_Subsets_Medium/index.js
+++ 78_Subsets_Medium/index.js
@@ -3,5 +3,20 @@
  * @return {number[][]}
  */
 var subsets = function (nums) {
+    let result = [];
 
+    for (let i = 0; i < nums.length; i++) {
+        let temp = [];
+        result.forEach((item) => temp.push(item.concat([nums[i]])));
+        temp.push([nums[i]]);
+        // console.log(temp);
+        result = result.concat(temp);
+        // console.log(result);
+    }
+
+    result.push([]);
+
+    return result;
 };
+
+console.log(subsets([1, 2, 3]));
