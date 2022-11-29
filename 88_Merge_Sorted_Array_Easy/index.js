0f35065ed05db1faab50c4d9f79bc0833c32e5c7
Wonjong Park <wonjong2@gmail.com>
2022-11-29 10:48:27 -0800

    Updates the solution

--- 88_Merge_Sorted_Array_Easy/index.js
+++ 88_Merge_Sorted_Array_Easy/index.js
@@ -6,5 +6,9 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function (nums1, m, nums2, n) {
-
+    nums1.splice(m, nums1.length - m + 1, ...nums2);
+    nums1.sort((a, b) => a - b);
+    return nums1;
 };
+
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3));
